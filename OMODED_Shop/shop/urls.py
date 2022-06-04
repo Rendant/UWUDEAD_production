21bbadbd0b3c279f2c7eae92086ed0269fa4560b
Gleb Yaurov <yaurov159@gmail.com>
2022-06-04 18:30:01 +0700

    перевёл почти все представления в generic

--- OMODED_Shop/shop/urls.py
+++ OMODED_Shop/shop/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from .views import *
 
 
 urlpatterns = [
     path('', index, name='home'),
     path('collections/', Collections.as_view(), name='collections'),
-    path('collections/<slug:collection_slug>/', collection, name='collection'),
-    path('collections/<slug:collection_slug>/product/<slug:good_slug>/', good, name='good')
+    path('collections/<slug:collection_slug>/', Collection.as_view(), name='collection'),
+    path('collections/<slug:collection_slug>/product/<slug:good_slug>/', Good.as_view(), name='good')
 ]
--- OMODED_Shop/shop/views.py
+++ OMODED_Shop/shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 def index(request):
@@ -10,16 +10,26 @@ def index(request):
 
 class Collections(ListView):
     model = Collections
-    template_name = 'shop/collections.html'
+    template_name = 'shop/collections_list.html'
 
 
-def collection(request, collection_slug):
-    filtered_goods = Goods.objects.filter(collection__slug=collection_slug)
-    context = {
-        'filtered_goods': filtered_goods
-    }
-    return render(request, template_name='shop/collection.html', context=context)
+class Collection(ListView):
+    model = Goods
+    template_name = 'shop/collection_list.html'
+    context_object_name = 'filtered_goods'
+
+    def get_queryset(self):
+        return Goods.objects.filter(collection__slug=self.kwargs['collection_slug'])
 
 
-def good(request, collection_slug, good_slug):
-    return HttpResponse('<h1>Hello good</h1>')
+class Good(DetailView):
+    model = Goods
+    template_name = 'shop/good_detail.html'
+    context_object_name = 'good'
+
+    def get_queryset(self):
+        collection = self.kwargs.get('collection_slug', '')
+        q = super().get_queryset()
+        return q.filter(collection__slug=collection).select_related('collection')
+# def good(request, collection_slug, good_slug):
+#     return HttpResponse('<h1>Hello good</h1>')
