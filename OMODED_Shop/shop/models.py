03e889b2d180feae52984ecefbcc00a9e20ea1b2
Rox <sllim03@mai.ru>
2022-06-06 15:43:59 +0700

    обновил модельки

--- OMODED_Shop/shop/models.py
+++ OMODED_Shop/shop/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
+from django.http import request
 
 
 class Goods(models.Model):
@@ -11,7 +13,7 @@ class Goods(models.Model):
                                verbose_name='Цена')
     collection = models.ManyToManyField('Collections', verbose_name='Коллекция')
     size_s = models.PositiveSmallIntegerField(editable=True, blank=True, null=True,  verbose_name='Размер S')
-    size_m = models.PositiveSmallIntegerField(editable=True, blank=True, null=True,verbose_name='Размер M')
+    size_m = models.PositiveSmallIntegerField(editable=True, blank=True, null=True, verbose_name='Размер M')
     size_l = models.PositiveSmallIntegerField(editable=True, blank=True, null=True, verbose_name='Размер L')
     size_xl = models.PositiveSmallIntegerField(editable=True, blank=True, null=True, verbose_name='Размер XL')
 
@@ -19,7 +21,11 @@ class Goods(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('good', kwargs={'good_slug': self.slug})
+        # now_path = request.HttpRequest.get_full_path()
+        # collection_slug = str(now_path).split('/')[-2]
+        collection = [str(col) for col in self.collection.all()]
+        collection_slug = slugify(collection[0])
+        return reverse('good', kwargs={'good_slug': self.slug, 'collection_slug': collection_slug})
 
     class Meta:
         verbose_name = 'Товар'
@@ -31,6 +37,7 @@ class Collections(models.Model):
     name = models.CharField(max_length=50, unique=True,
                             verbose_name='Наименование коллекции')
     slug = models.SlugField(unique=True)
+    photo = models.ImageField(upload_to='img/collections', blank=True)
 
     def __str__(self):
         return self.name
@@ -47,7 +54,7 @@ class Collections(models.Model):
 class Photos(models.Model):
     good = models.ForeignKey(Goods, on_delete=models.PROTECT,
                              verbose_name='Товар')
-    photo = models.ImageField(upload_to='photos/%y/%m/%d/',
+    photo = models.ImageField(upload_to='photos/%m/',
                               verbose_name='Фото')
 
     def __str__(self):
