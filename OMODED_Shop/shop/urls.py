6e2fb67e7d65af2755996b688ed13aa6961167a0
Gleb Yaurov <yaurov159@gmail.com>
2022-06-08 18:28:40 +0700

    добавил ajax в поиск(выводиться криво)

--- OMODED_Shop/shop/urls.py
+++ OMODED_Shop/shop/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('collections/', Collections.as_view(), name='collections'),
-    path('search/', Search.as_view(), name='search'),
+    path('search/', search, name='search'),
     path('collections/<slug:collection_slug>/', Collection.as_view(), name='collection'),
     path('collections/<slug:collection_slug>/product/<slug:good_slug>/', Good.as_view(), name='good')
 ]
