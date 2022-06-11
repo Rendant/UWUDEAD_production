8c3a712fbe11aea283e0fcff8624f23f536a3337
Gleb Yaurov <yaurov159@gmail.com>
2022-06-11 23:35:19 +0700

    что то поменял

--- OMODED_Shop/shop/admin.py
+++ OMODED_Shop/shop/admin.py
@@ -3,11 +3,19 @@ from .models import *
 
 
 class GoodsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description',
-                    'cost', 'size_s', 'size_m',
-                    'size_l', 'size_xl')
+    list_display = ('id', 'name', 'cost', 'is_available',
+                    'size_s', 'size_m', 'size_l', 'size_xl')
     filter_horizontal = ('collection', )
     list_display_links = ('id', 'name')
+    search_fields = ('name', 'description')
+    list_filter = ('collection',)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+class GoodsNCAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'cost', 'is_available',
+                    'quantity')
+    filter_horizontal = ('collection', )
     search_fields = ('name', 'description')
     list_filter = ('collection',)
     prepopulated_fields = {"slug": ("name",)}
@@ -21,5 +29,13 @@ class CollectionsAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
 
+class StylesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'good_name')
+    list_display_links = ('id', 'good_name')
+    search_fields = ('good_name',)
+
+
 admin.site.register(Goods, GoodsAdmin)
+admin.site.register(GoodsNC, GoodsNCAdmin)
 admin.site.register(Collections, CollectionsAdmin)
+admin.site.register(Styles, StylesAdmin)
