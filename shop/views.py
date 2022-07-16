ed4d328ad3f4ea0d0b7431f6bf03c2711d494f30
Gleb Yaurov <yaurov159@gmail.com>
2022-07-17 01:07:16 +0700

    Теперь после входа, редирект на прошлую страницу

--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.views.generic import ListView, DetailView
 from django.template.loader import render_to_string
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseRedirect
 from .forms import NewUserForm, LoginForm, ResetForm
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, PasswordResetView
@@ -95,6 +95,9 @@ def register_request(request):
 
 class LoginRequest(LoginView):
     form_class = LoginForm
+
+    def get_default_redirect_url(self):
+        return self.request.META.get('HTTP_REFERER', '/')
 
 
 class ResetRequest(PasswordResetView):
