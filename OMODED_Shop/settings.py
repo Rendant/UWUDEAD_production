c168e3c031509ae2a382ebdb7bbdb17bd86db8f2
yaurov <yaurov159@gmail.com>
2022-10-01 11:07:45 +0700

    поиск и скролл в поиске

--- OMODED_Shop/settings.py
+++ OMODED_Shop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure--ic77z$ya0i+nb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
